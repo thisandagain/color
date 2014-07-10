8a479e66ede5734fcb4a7517c517eb8213175703
pixelslip <pixelslip@gmail.com>
2014-07-10 16:24:35 +0200

    Add color operation methods
    A rewrite of the offsetWith method. Smaller, easier.

--- Tests/EDColorHSLTest.m
+++ Tests/EDColorHSLTest.m
@@ -128,4 +128,17 @@
     STAssertEqualsWithAccuracy(alpha, 0.75f, TEST_ACCURACY, nil);
 }
 
+- (void)testSpinOffset
+{
+    UIColor *base   = [UIColor colorWithRed:1.0f green:0.0f blue:0.0f alpha:1.0f];
+    UIColor *color  = [base spin:120.0f];
+    
+    SPLIT_RESULT_TO_RGBA
+    
+    STAssertEqualsWithAccuracy(red, 0.0f, TEST_ACCURACY, nil);
+    STAssertEqualsWithAccuracy(green, 1.0f, TEST_ACCURACY, nil);
+    STAssertEqualsWithAccuracy(blue, 0.0f, TEST_ACCURACY, nil);
+    STAssertEqualsWithAccuracy(alpha, 1.0f, TEST_ACCURACY, nil);
+}
+
 @end
